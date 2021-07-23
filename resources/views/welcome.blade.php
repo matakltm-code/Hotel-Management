1712086b5082b7963503768e0f935572dec6fbec
Micheal Ataklt <matakltm.code@gmail.com>
2021-07-23 01:20:37 -0700

    Layout modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -189,8 +189,7 @@
 
         <!-- Copyright -->
         <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-            © 2021 Copyright:
-            <a class="text-reset fw-bold" href="/">AG Hotel</a>
+            © 2021 Copyright:<a class="text-reset fw-bold" href="/">{{ config('app.name') }}</a>
         </div>
         <!-- Copyright -->
     </footer>
